2a9daa5002dddb90494d7a89959c41c0cd500f54
Alexander Blinn <64211323+aqua-lx@users.noreply.github.com>
2022-12-22 21:43:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,80 @@
 # -*- coding: utf-8 -*-
-"""
-In order to access the API of the Marktstammdatenregister, registration as a Webdienstnutzer/Marktakteur is required.
-Link: https://test.marktstammdatenregister.de/MaStR
-After registration a marktakteurMastrNummer and an apiKey are available for use.
-"""
+"""A script to demonstrate how to use the MarktstammdatenregisterAPI class."""
 
+from typing import Any
 
 from helpers.api_accessor import MarktstammdatenregisterAPI
-from helpers.enumeration import *
+from helpers.enumeration import Energietraeger, Marktfunktion, MarktTyp
 
 
+def get_power_units(**kwargs: dict[str, str]) -> dict[str, Any]:
+    """
+    Get data of power units from the Marktstammdatenregister API.
 
-def get_power_units(**kwargs):
-    api = MarktstammdatenregisterAPI(MarketType.ANLAGE.value)
+    Parameters
+    ----------
+    **kwargs : dict[str, str]
+        Additional keyword arguments to pass to the API method.
+
+    Returns
+    -------
+    dict[str, Any]
+        The data returned by the API method, serialized as a dictionary.
+    """
+    api = MarktstammdatenregisterAPI(MarktTyp.ANLAGE.value)
     return api.get('GetGefilterteListeStromErzeuger', **kwargs)
 
 
-def get_market_players(**kwargs):
-    api = MarktstammdatenregisterAPI(MarketType.AKTEUR.value)
+def get_market_players(**kwargs: dict[str, str]) -> dict[str, Any]:
+    """Get data of market players from the Marktstammdatenregister API.
+
+    Parameters
+    ----------
+    **kwargs : dict[str, str]
+        Additional keyword arguments to pass to the API method.
+
+    Returns
+    -------
+    dict[str, Any]
+        The data returned by the API method, serialized as a dictionary.
+    """
+    api = MarktstammdatenregisterAPI(MarktTyp.AKTEUR.value)
     return api.get('GetGefilterteListeMarktakteure', **kwargs)
 
 
-def get_player_info(mastrNummer: str, **kwargs):
-    api = MarktstammdatenregisterAPI(MarketType.AKTEUR.value)
-    return api.get('GetMarktakteur', mastrNummer=mastrNummer, **kwargs)
+def get_player_info(mastr_nummer: str, **kwargs: dict[str, str]
+                    ) -> dict[str, Any]:
+    """
+    Get information about a market player from the Marktstammdatenregister API.
+
+    Parameters
+    ----------
+    mastr_nummer : str
+        The MaStR number of the market player.
+    **kwargs : dict[str, str]
+        Additional keyword arguments to pass to the API method.
+
+    Returns
+    -------
+    dict[str, Any]
+        The data returned by the API method, serialized as a dictionary.
+    """
+    api = MarktstammdatenregisterAPI(MarktTyp.AKTEUR.value)
+    return api.get('GetMarktakteur', mastrNummer=mastr_nummer, **kwargs)
 
 
-power_units = get_power_units(ort='Schiffweiler',
-                              energietraeger=Energietraeger.WIND.value)
+if __name__ == '__main__':
+    power_units = get_power_units(
+        ort='Schiffweiler',
+        energietraeger=Energietraeger.WIND.value
+        )
 
-market_akteurs = get_market_players(ort='Trier',
-                                    # marktrollen=Marktfunktion.STROMNETZBETREIBER.value
-                                    )
+    market_akteurs = get_market_players(
+        ort='Trier',
+        marktfunktion=Marktfunktion.STROMNETZBETREIBER.value
+        )
 
-player_info = get_player_info('SNB966813503780')
+    player_info = get_player_info(mastr_nummer='SNB966813503780')
 
 # %%
 # import csv
